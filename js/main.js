f31d69d5257a5dcc5694c13350949f0d5496b0c0
kate-prisyajna <kate.prisyajna@gmail.com>
2018-12-10 21:45:49 +0200

    исправлены ошибки

--- js/main.js
+++ js/main.js
@@ -12,9 +12,7 @@ function myFunction() {
   }
 }
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+{
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("myBtn").style.display = "block";
   } else {
