2fe92152f52da4ad81f465726dc43afcfe5c2219
kate-prisyajna <kate.prisyajna@gmail.com>
2018-12-11 11:20:21 +0200

    fixed products and scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction(); scrollFunction()};
 
 var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
@@ -12,14 +12,14 @@ function myFunction() {
   }
 }
 
-{
+function scrollFunction() {
+
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("myBtn").style.display = "block";
   } else {
     document.getElementById("myBtn").style.display = "none";
   }
 }
-
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
